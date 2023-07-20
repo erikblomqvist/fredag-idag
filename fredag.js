2eced83049e47eb026bbc0b89429c869db726717
erikblomqvist-huma <erik@hu.ma>
2023-07-20 17:09:38 +0200

    Change to express server

--- fredag.js
+++ fredag.js
@@ -1,22 +1,28 @@
 require('dotenv').config();
-const { App, LogLevel } = require('@slack/bolt');
+const { App, LogLevel } = require("@slack/bolt");
+const express = require('express');
 
 const app = new App({
-    token: process.env.TOKEN,
-    signingSecret: process.env.SIGNING_SECRET,
-    logLevel: LogLevel.DEBUG,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  logLevel: LogLevel.DEBUG
 });
 
 app.message('Är det fredag idag?', async ({ message, say }) => {
-    const d = new Date();
-    const isFriday = (d.getDay() === 5) ? "Ja, mina bekanta. :fredag_mina_bekanta:" : "Nej. :fredag-idag:";
-
-    await say(`Hej <@${message.user}>! ${isFriday}`);
+  const d = new Date();
+  const isFriday = (d.getDay() === 5) ? "Ja, mina bekanta. :fredag_mina_bekanta:" : "Nej. :fredag-idag:";
+  await say(`Hej <@${message.user}>! ${isFriday}`);
 });
 
 (async () => {
-    // Start your app
-    await app.start(process.env.PORT || 3000);
+  const server = express();
 
-    console.log('⚡️ Bolt app is running!');
+  server.all('/slack/events', (req, res) => {
+    res.status(200).send('Ok');
+  });
+
+  server.listen(process.env.PORT || 3000);
+
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
 })();
