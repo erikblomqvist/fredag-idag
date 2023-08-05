5dcc4f49df85648b4ada7014d083a51d013dcb05
erikblomqvist-huma <erik@hu.ma>
2023-08-05 13:09:58 +0200

    Fix broken fredag.js

--- fredag.js
+++ fredag.js
@@ -13,29 +13,29 @@ const app = new App({
 const checkFriday = d => d.day() === 5
 
 app.message('Är det fredag idag?', async ({ say }) => {
-    const d = moment().tz('Europe/Oslo')
+    const d = moment().tz('Europe/Oslo');
 
-    const isFriday = checkFriday(d)
-    let response
+    const isFriday = checkFriday(d);
+    let response;
 
     if (isFriday) {
-        response = "Ja, mina bekanta. :fredag_mina_bekanta:"
+        response = "Ja, mina bekanta. :fredag_mina_bekanta:";
     } else {
-        response = "Nej. :fredag-idag:"
+        response = "Nej. :fredag-idag:";
     }
 
-    await say(response)
-})
+    await say(response);
+});
 
 app.message(/Varför\?/i, async ({ message, client, say }) => {
     try {
-        // const d = moment().tz('Europe/Oslo')
+        // const d = moment().tz('Europe/Oslo');
         // const isFriday = checkFriday(d)
-        const isFriday = true // Hardcoded for testing
+        const isFriday = true; // Hardcoded for testing
 
         if (isFriday) {
-            const haeljaFile = 'https://github.com/erikblomqvist/fredag-idag/raw/main/haelj.m4a'
-            const fileResponse = await axios.get(haeljaFile, { responseType: 'arraybuffer' })
+            const haeljaFile = 'https://github.com/erikblomqvist/fredag-idag/raw/main/haelj.m4a';
+            const fileResponse = await axios.get(haeljaFile, { responseType: 'arraybuffer' });
 
             await client.files.upload({
                 channels: message.channel,
@@ -46,15 +46,15 @@ app.message(/Varför\?/i, async ({ message, client, say }) => {
                         contentType: 'audio/m4a'
                     }
                 }
-            })
+            });
         } else {
-            await say(':sob: :sob: :banana:')
+            await say(':sob: :sob: :banana:');
         }
     } catch (error) {
-        console.error('Error in Varför message handler: ', error)
-        await say('Sorry, something went wrong.')
+        console.error('Error in Varför message handler: ', error);
+        await say('Sorry, something went wrong.');
     }
-})
+});
 
 (async () => {
     const server = express()
