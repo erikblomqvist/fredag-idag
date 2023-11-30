dd826fb76c0709b1a4b44e45d7ed584a79a3f24b
erikblomqvist-huma <erik@hu.ma>
2023-11-30 11:12:05 +0100

    Add :69: emoji reaction

--- fredag.js
+++ fredag.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
-const { App, LogLevel } = require("@slack/bolt")
+const { App, LogLevel, reactionAdded } = require("@slack/bolt")
 const express = require('express')
 const moment = require('moment-timezone')
 const axios = require('axios')
-const { Readable } = require('stream');
+const { Readable } = require('stream')
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -14,48 +14,48 @@ const app = new App({
 const checkFriday = d => d.day() === 5
 
 app.message('Är det fredag idag?', async ({ say }) => {
-    const d = moment().tz('Europe/Oslo');
+    const d = moment().tz('Europe/Oslo')
 
-    const isFriday = checkFriday(d);
-    let response;
+    const isFriday = checkFriday(d)
+    let response
 
     if (isFriday) {
-        response = "Ja, mina bekanta. :fredag_mina_bekanta:";
+        response = "Ja, mina bekanta. :fredag_mina_bekanta:"
     } else {
-        response = "Nej. :fredag-idag:";
+        response = "Nej. :fredag-idag:"
     }
 
-    await say(response);
-});
+    await say(response)
+})
 
 app.message('Röven!', async ({ say }) => {
-    const d = moment().tz('Europe/Oslo');
+    const d = moment().tz('Europe/Oslo')
 
-    const isFriday = checkFriday(d);
-    let response;
+    const isFriday = checkFriday(d)
+    let response
 
     if (isFriday) {
-        response = "Lugna ner dig nu, det är ju fredag.' :fredag_mina_bekanta:";
+        response = "Lugna ner dig nu, det är ju fredag.' :fredag_mina_bekanta:"
     } else {
-        response = "Lugna ner dig nu, kukunge. :shushing_face: :pelle_big_black_hat:";
+        response = "Lugna ner dig nu, kukunge. :shushing_face: :pelle_big_black_hat:"
     }
 
-    await say(response);
-});
+    await say(response)
+})
 
 app.message(/Varför\?/i, async ({ message, client, say }) => {
     try {
-        const d = moment().tz('Europe/Oslo');
-        const isFriday = checkFriday(d);
+        const d = moment().tz('Europe/Oslo')
+        const isFriday = checkFriday(d)
 
         if (isFriday) {
-            const haeljaFile = 'https://github.com/erikblomqvist/fredag-idag/raw/main/haelj.m4a';
-            const fileResponse = await axios.get(haeljaFile, { responseType: 'arraybuffer' });
+            const haeljaFile = 'https://github.com/erikblomqvist/fredag-idag/raw/main/haelj.m4a'
+            const fileResponse = await axios.get(haeljaFile, { responseType: 'arraybuffer' })
 
             // Convert buffer to readable stream
-            const fileStream = new Readable();
-            fileStream.push(fileResponse.data);
-            fileStream.push(null);
+            const fileStream = new Readable()
+            fileStream.push(fileResponse.data)
+            fileStream.push(null)
 
             await client.files.upload({
                 channels: message.channel,
@@ -63,15 +63,39 @@ app.message(/Varför\?/i, async ({ message, client, say }) => {
                 filename: 'Nå ære Hælja.m4a',
                 filetype: 'audio/m4a',
                 title: 'Nå ære Hælja'
-            });
+            })
         } else {
-            await say(':sob: :sob: :banana:');
+            await say(':sob: :sob: :banana:')
         }
     } catch (error) {
-        console.error('Error in Varför message handler: ', error);
-        await say('Sorry, something went wrong.');
+        console.error('Error in Varför message handler: ', error)
+        await say('Sorry, something went wrong.')
     }
-});
+})
+
+app.event('reaction_added', async ({ event, client }) => {
+    try {
+        if(event.reaction === '69') {
+            const oneDayAgo = moment().subtract(1, 'days').unix()
+            const history = await client.conversations.history({
+                channel: event.item.channel,
+                oldest: oneDayAgo.toString()
+            })
+
+            const reactedMessage = history.messages.find(m => m.ts === event.item.ts)
+
+            if(reactedMessage) {
+                await client.reactions.add({
+                    channel: event.item.channel,
+                    timestamp: event.item.ts,
+                    name: '69'
+                })
+            }
+        }
+    } catch (error) {
+        console.error('Error in reaction_added event handler: ', error)
+    }
+})
 
 (async () => {
     const server = express()
